515342fb6b06284d9267916d916a91139a90778d
abdulboss240-commits <abdulboss240@gmail.com>
2026-06-24 18:21:53 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // GitHub Pages ke liye base path set kiya gaya hai
+export default defineConfig({
   base: "/ak-farms/",
-  
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
